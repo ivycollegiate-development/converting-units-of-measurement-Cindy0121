cc305c263292ac9875a99399b5b1f3e4b430b898
Cindy0121 <cchen25_student@ivycollegiate.org>
2024-09-26 06:01:31 +0000

    add kg to pounds and pounds to kg

--- Cindy_Sprint_Projectv1.6/Cindy_Sprint_Projectv1.6.py
+++ Cindy_Sprint_Projectv1.6/Cindy_Sprint_Projectv1.6.py
@@ -1,6 +1,8 @@
 def print_menu():
     print('1. Kilometers to Miles')
     print('2. Miles to Kilometers')
+    print('3. Kilograms to Pounds')
+    print('4. Pounds to Kilograms')
 
 def km_miles():
     km = float(input("Please enter distance in kilometers: "))
@@ -12,6 +14,16 @@ def miles_km():
     km = miles*1.609
     print("Distance in kilometers: {0}".format(km))
 
+def kg_lb():
+    kg = float(input("Please enter weight in kilograms: "))
+    pounds = kg/2.20462262
+    print("Weight in pounds: {0}".format(pounds))
+
+def lb_kg():
+    pounds = float(input("Please enter weight in pounds: "))
+    kg = pounds*2.20462262
+    print("weight in kilograms: {0}".format(kg))
+
 if __name__ == '__main__':
     print_menu()
     choice = input("Which conversion would you like to do today?: ")
@@ -19,3 +31,7 @@ if __name__ == '__main__':
         km_miles()
     if choice == '2':
         miles_km()
+    if choice == '3':
+        kg_lb()
+    if choice == '4':
+        lb_kg()
